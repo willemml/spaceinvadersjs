a3d597adc1369a2fb935d03ab81d29abfbf82491
Willem Leitso <willem@Furnace.local>
2019-04-08 22:34:25 +0200

    Added a lot of comments

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// Defining variables and arrays
 var player
 var objs = []
 var walls = []
 var bullets = []
 var crashloc
-var canvdim = [1600, 900]
+var canvdim = [1600, 900] // screen resolution
 var timeOfLastShot = 0\
 var numOfObjs = canvdim[0] / 100
 var i
@@ -11,6 +12,7 @@ var u
 var o
 
 function startGame() {
+  // Creating visible game objects
   player = new Component(50, 50, 'green', canvdim[0] / 2, canvdim[1] / 4 * 3)
   walls[0] = new Component(1, canvdim[1], 'black', -1, 0)
   walls[1] = new Component(1, canvdim[1], 'black', canvdim[0], 0)
@@ -21,6 +23,7 @@ function startGame() {
   myGameArea.start()
 }
 
+// This displays a message in the center of the canvas
 function message(msg) {
   var ctx = myGameArea.canvas.getContext("2d");
   ctx.font = "30px Courier";
@@ -30,21 +33,25 @@ function message(msg) {
   ctx.fillText(textString, (canvdim[0] / 2) - (textWidth / 2), canvdim[1] / 2);
 }
 
+// Creates a bullet
 function shoot(xstart, ystart) {
   bullets.push(new Component(10, 10, 'blue', xstart - 5, ystart - 5))
   console.log('BANG')
 }
 
+// Checks if a bullet can be created and creates one if yes
 function button() {
-  if (myGameArea.keys && myGameArea.keys[32] && new Date() - timeOfLastShot > 250) {
+  if (myGameArea.keys && myGameArea.keys[32] && new Date() - timeOfLastShot > 250 /* Time in ms between shots */) {
     shoot(player.x + (player.width / 2), player.y - 6)
     timeOfLastShot = new Date()
   }
 }
+
 // Check if bullet went offscreen
 function checkShotGone(bullet, num) {
   bullet.splice(bullet[num], 1)
 }
+
 // Check if bullet hit something
 function checkShotHit(bullet, num, object, onum) {
   if (object[onum].crashWith(bullet[num])) {
@@ -54,6 +61,7 @@ function checkShotHit(bullet, num, object, onum) {
   }
 }
 
+// Creates canvas
 var myGameArea = {
   canvas: document.createElement('canvas'),
   start: function() {
@@ -78,6 +86,7 @@ var myGameArea = {
   }
 }
 
+// Creates a rectangle of a color at a point
 function Component(width, height, color, x, y) {
   this.color = color
   this.width = width
@@ -96,6 +105,7 @@ function Component(width, height, color, x, y) {
     this.x += this.speedX
     this.y += this.speedY
   }
+  // collision detection
   this.crashWith = function(otherobj) {
     var myleft = this.x
     var myright = this.x + (this.width)
@@ -133,10 +143,12 @@ function Component(width, height, color, x, y) {
   }
 }
 
+// updates all game objects and checks for movements (arrow keys), collisions, shots (shooting bullets with space) and hits
 function updateGameArea() {
   var moveSpeed = 0.5
   var uncrashSpeed = moveSpeed * (objs.length + bullets.length + walls.length - 1)
   for (o = 0; o < walls.length; o++) {
+    // player / wall collisions
     if (player.crashWith(walls[o])) {
       switch (crashloc) {
         case 'right':
